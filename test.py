b345dd50bb958f61c23844e281ae2d065c92abe9
basicworld <basicworld@163.com>
2017-02-06 13:29:03 +0800

    debug

---
+++ test.py
@@ -0,0 +1,11 @@
+import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')  # 编译环境utf8
+
+
+from text_content_parse import text_parse
+
+if __name__ == '__main__':
+    print text_parse('test')
+    print text_parse('你好')
